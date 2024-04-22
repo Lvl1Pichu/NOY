5eca2de17065ff03931f9d73df25fbe38f2fa468
Linus Hammarberg <Linus.hammarberg@gmail.com>
2024-04-22 10:46:56 +0200

    Revert "Merge branch 'Implement-Jest-Testing'"
    This reverts commit d00eebfe95e28b069c4473b0747f979ccac3f3a8, reversing
    changes made to 7f1cfebdaad02e06387fd60b192487d2d6d41e13.

--- src/Tests/Sidebar.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import Sidebar from "../Sidebar";
-
-test("renders the sidebar", () => {
-  render(<Sidebar isOpen={true} onClose={() => {}} />);
-  const linkElement = screen.getByText(/WE/i);
-  expect(linkElement).toBeInTheDocument();
-});
